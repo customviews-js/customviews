2b221f4158937fb3332b5ecda168f7235e3648d3
gerteck <changerteck@gmail.com>
2025-10-24 18:58:58 +0800

    Site Update. [skip ci]

--- _markbind/plugins/customviews.js
+++ _markbind/plugins/customviews.js
@@ -1,16 +1,17 @@
 /**
  * CustomViews Plugin for MarkBind
  * Injects the CustomViews auto-init script into every page.
- * Configuration is loaded from /customviews.config.json
+ * Configuration is loaded from websiteBaseUrl/customviews.config.json
  */
-// '<script src="../../../dist/custom-views.min.js" data-base-url="/customviews"></script>'
 
-// this is in ES module format
+// Local Development: ESM Modules, relative directory
+// Markbind Deployment: Use unpkg CDN
 function getScripts() {
   return [
+    // '<script src="../../../dist/custom-views.min.js" data-base-url="/customviews"></script>',
     '<script src="https://unpkg.com/@customviews-js/customviews" data-base-url="/customviews"></script>'
   ];
 };
 
-// module.exports = { getScripts };
+// CJS: module.exports = { getScripts };
 export { getScripts };
