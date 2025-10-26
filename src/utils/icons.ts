11f2dc55a9cd65f023068519dd38535f99aef07b
gerteck <changerteck@gmail.com>
2025-10-26 16:15:44 +0800

    Update SVG icons to use currentColor for better theming support

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -35,7 +35,7 @@ export function getResetIcon(): string {
  * Copy icon for sharing URL button
  */
 export function getCopyIcon(): string {
-  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18" version="1.1">
+  return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18" version="1.1" fill="currentColor">
               <g id="surface1">
                 <path d="M 11.273438 0 L 2.546875 0 C 1.746094 0 1.089844 0.613281 1.089844
                 1.363281 L 1.089844 10.910156 L 2.546875 10.910156 L 2.546875 1.363281 L 11.273438
@@ -49,75 +49,65 @@ export function getCopyIcon(): string {
 }
 
 export function getTickIcon(): string {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="2 2 22 22">
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="2 2 22 22" fill="currentColor">
             <path d="M 19.28125 5.28125 L 9 15.5625 L 4.71875 11.28125 L 3.28125 12.71875 L 8.28125 17.71875 
             L 9 18.40625 L 9.71875 17.71875 L 20.71875 6.71875 Z"></path>
           </svg>`;
 }
 
 export function getNavHeadingOnIcon(): string {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181" fill="none">
-            <mask id="path-1-inside-1_1312_2942" fill="white">
-            <rect y="34.5001" width="250" height="146" rx="4"/>
-            </mask>
-            <rect y="34.5001" width="250" height="146" rx="4" stroke="black" stroke-width="10" mask="url(#path-1-inside-1_1312_2942)"/>
-            <line x1="27" y1="62.0001" x2="77" y2="62.0001" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="77.8888" x2="77" y2="77.8888" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="97.4454" x2="221" y2="97.4454" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="150" x2="221" y2="150" stroke="black" stroke-width="5"/>
-            <line x1="247.5" y1="43.0001" x2="247.5" y2="13.0001" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M185 12.5001L247 12.5001" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <line x1="204.09" y1="36.6095" x2="181.698" y2="10.0228" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M125 9.50012L181 9.50012" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M144.305 35.2579L120.095 6.56679" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M120 6.50037L64 6.50037" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M87.1957 36.1024L59 2.50008" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M59 2.50037L3 2.50037" stroke="black" stroke-width="5" stroke-linecap="round"/>
-            <path d="M2.5 38.5001L2.5 3.00012" stroke="black" stroke-width="5" stroke-linecap="round"/>
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181">
+            <rect y="34.5001" width="250" height="146" rx="4" stroke="currentColor" stroke-width="10" fill="none"/>
+            <line x1="27" y1="62.0001" x2="77" y2="62.0001" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="77.8888" x2="77" y2="77.8888" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="97.4454" x2="221" y2="97.4454" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="150" x2="221" y2="150" stroke="currentColor" stroke-width="5"/>
+            <line x1="247.5" y1="43.0001" x2="247.5" y2="13.0001" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M185 12.5001L247 12.5001" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <line x1="204.09" y1="36.6095" x2="181.698" y2="10.0228" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M125 9.50012L181 9.50012" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M144.305 35.2579L120.095 6.56679" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M120 6.50037L64 6.50037" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M87.1957 36.1024L59 2.50008" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M59 2.50037L3 2.50037" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+            <path d="M2.5 38.5001L2.5 3.00012" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
           </svg>`;
 }
 
 export function getNavHeadingOffIcon(): string {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181" fill="none">
-            <mask id="path-1-inside-1_1312_2984" fill="white">
-            <rect y="34.5" width="250" height="146" rx="4"/>
-            </mask>
-            <rect y="34.5" width="250" height="146" rx="4" stroke="black" stroke-width="10" mask="url(#path-1-inside-1_1312_2984)"/>
-            <line x1="27" y1="62" x2="77" y2="62" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="77.8887" x2="77" y2="77.8887" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="97.4453" x2="221" y2="97.4453" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="150" x2="221" y2="150" stroke="black" stroke-width="5"/>
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181" fill="currentColor">
+            <rect y="34.5001" width="250" height="146" rx="4" stroke="currentColor" stroke-width="10" fill="none"/>
+            <line x1="27" y1="62" x2="77" y2="62" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="77.8887" x2="77" y2="77.8887" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="97.4453" x2="221" y2="97.4453" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="150" x2="221" y2="150" stroke="currentColor" stroke-width="5"/>
           </svg>`;
 }
 
 /**
  * Transition Icon for Navigation Headings
- * @returns 
  */
 export function getNavDashed(): string {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181" fill="none">
-            <mask id="path-1-inside-1_1312_2897" fill="white">
-            <rect y="34.5001" width="250" height="146" rx="4"/>
-            </mask>
-            <rect y="34.5001" width="250" height="146" rx="4" stroke="black" stroke-width="10" mask="url(#path-1-inside-1_1312_2897)"/>
-            <line x1="27" y1="62.0001" x2="77" y2="62.0001" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="77.8888" x2="77" y2="77.8888" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="97.4454" x2="221" y2="97.4454" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="black" stroke-width="5"/>
-            <line x1="27" y1="150" x2="221" y2="150" stroke="black" stroke-width="5"/>
-            <path d="M245 37.0001V39.5001H250V37.0001H247.5H245ZM250 13.0001C250 11.6194 248.881 10.5001 247.5 10.5001C246.119 10.5001 245 11.6194 245 13.0001H247.5H250ZM250 31.0001C250 29.6194 248.881 28.5001 247.5 28.5001C246.119 28.5001 245 29.6194 245 31.0001H247.5H250ZM245 19.0001C245 20.3808 246.119 21.5001 247.5 21.5001C248.881 21.5001 250 20.3808 250 19.0001H247.5H245ZM247.5 37.0001H250V31.0001H247.5H245V37.0001H247.5ZM247.5 19.0001H250V13.0001H247.5H245V19.0001H247.5Z" fill="black"/>
-            <line x1="204.09" y1="36.6095" x2="181.698" y2="10.0228" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M125 9.50012L181 9.50012" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M144.305 35.2579L120.095 6.56679" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M120 6.50037L64 6.50037" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M87.1957 36.1024L59 2.50008" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M59 2.50037L3 2.50037" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M2.5 38.5001L2.5 3.00012" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
-            <path d="M185 12.5001L247 12.5001" stroke="black" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="250" height="181" viewBox="0 0 250 181">
+            <rect y="34.5001" width="250" height="146" rx="4" stroke="currentColor" stroke-width="10" fill="none"/>
+            <line x1="27" y1="62.0001" x2="77" y2="62.0001" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="77.8888" x2="77" y2="77.8888" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="97.4454" x2="221" y2="97.4454" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="114.555" x2="221" y2="114.555" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="132.889" x2="221" y2="132.889" stroke="currentColor" stroke-width="5"/>
+            <line x1="27" y1="150" x2="221" y2="150" stroke="currentColor" stroke-width="5"/>
+            <path d="M245 37.0001V39.5001H250V37.0001H247.5H245ZM250 13.0001C250 11.6194 248.881 10.5001 247.5 10.5001C246.119 10.5001 245 11.6194 245 13.0001H247.5H250ZM250 31.0001C250 29.6194 248.881 28.5001 247.5 28.5001C246.119 28.5001 245 29.6194 245 31.0001H247.5H250ZM245 19.0001C245 20.3808 246.119 21.5001 247.5 21.5001C248.881 21.5001 250 20.3808 250 19.0001H247.5H245ZM247.5 37.0001H250V31.0001H247.5H245V37.0001H247.5ZM247.5 19.0001H250V13.0001H247.5H245V19.0001H247.5Z" fill="currentColor"/>
+            <line x1="204.09" y1="36.6095" x2="181.698" y2="10.0228" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M125 9.50012L181 9.50012" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M144.305 35.2579L120.095 6.56679" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M120 6.50037L64 6.50037" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M87.1957 36.1024L59 2.50008" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M59 2.50037L3 2.50037" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M2.5 38.5001L2.5 3.00012" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
+            <path d="M185 12.5001L247 12.5001" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-dasharray="10 10"/>
           </svg>`;
 }
